8ec47756343c0d9eabe7ca3cc84b806f5634ceb6
SergeyBondarenko <sergibondarenko@gmail.com>
2015-11-24 15:54:42 +0100

    AJAX works in tree menu

--- esquid/esearch/urls.py
+++ esquid/esearch/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
+from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie
 
 from . import views
 
 urlpatterns = [
 	#url(r'^$', views.index, name='index')
-	url(r'^$', views.IndexView.as_view(), name='index')
+	#url(r'^$', views.IndexView.as_view(), name='index'),
+	#url(r'^postmenu/$', ensure_csrf_cookie(views.postmenu), name='postmenu'),
+	url(r'^$', views.index, name='index'),
+	url(r'^postmenu/$', views.postmenu, name='postmenu'),
 ]
--- esquid/esearch/views.py
+++ esquid/esearch/views.py
@@ -2,28 +2,47 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.views import generic
 from django.core.urlresolvers import reverse
+from django.views.generic import TemplateView
+from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie, csrf_protect
 
 from .models import Esearch
 
 # Create your views here.
 Esearch = Esearch()
 
-class IndexView(generic.ListView):
-    template_name = 'esearch/index.html'
-    context_object_name = 'results_list'
+def index(request):
+	res = Esearch.search_all()
+	#return HttpResponse(res['hits']['hits'][0]['_source']['email'])
 
-    def get_queryset(self):
-        res = Esearch.search_all()
-        return res
+	first_name = []
+	for person in res['hits']['hits']:
+		first_name.append(person['_source'].get('first_name'))
+		
+	context = {'results_list': res, 'first_name': first_name}
+	return render(request, 'esearch/index.html', context)
 
-#def index(request):
-#	res = Esearch.search_all()
-#	#return HttpResponse(res['hits']['hits'][0]['_source']['email'])
+def postmenu(request):
+     if request.method == 'POST':
+         if request.is_ajax():
+             message = request.POST['msg']
+             return HttpResponse(str(message))
+             #return HttpResponse('Hello you!!!')
+         else:
+             raise Http404
+     else:
+         return HttpResponse('Nothing to return from server.')
+    
+#@csrf_exempt
+#class IndexView(generic.ListView):
+#    template_name = 'esearch/index.html'
+#    context_object_name = 'results_list'
 #
-#	first_name = []
-#	for person in res['hits']['hits']:
-#		first_name.append(person['_source'].get('first_name'))
-#		
-#	context = {'results_list': res, 'first_name': first_name}
-#	return render(request, 'esearch/index.html', context)
+#    #@csrf_exempt
+#    def get_queryset(self):
+#        res = Esearch.search_all()
+#        return res
+
+#def postmenu(request):
+#    return HttpResponse('Hello you!!!')
+
 	
