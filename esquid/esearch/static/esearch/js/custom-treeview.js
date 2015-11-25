097c96ba614e6f730c6940e528abeb6166e5b829
SergeyBondarenko <sergibondarenko@gmail.com>
2015-11-25 10:32:31 +0100

    Add search to menu and multi select.

--- esquid/esearch/static/esearch/js/custom-treeview.js
+++ esquid/esearch/static/esearch/js/custom-treeview.js
@@ -1,81 +1,92 @@
 $(function() {
 
-$.getJSON("/static/esearch/json/test_menu.json", function(menu_data){
-	console.log("Success: Menu json will be used.");
-})
-.done(function(menu_data){
-	var initSelectableTree = function() {
-	  return $('#treeview-selectable').treeview({
-	  	color: "#428bca",
-		data: JSON.stringify(menu_data),
-	    multiSelect: $('#chk-select-multi').is(':checked'),
-	    onNodeSelected: function(event, node) {
-	      $('#selectable-output').prepend('<p>' + node.text + ' was selected</p>');
-	
-		  // Send calls from menu to backend
-		  $.ajax({
-			type: 'POST',
-			url: 'postmenu/',
-			data: {
-				//msg: 'Hello my friend!',
-				msg: node.text,
-			},
-			success: function(response){
-				$('#selectable-output').html(response);
-				console.log(response);
-				//console.log('Success!');
-			},
-			error: function(response){
-				$('#selectable-output').html('Error (ajax post): Unable to receive data from server.');
-			}
-		  }); // ajax post
-	    },
-	    onNodeUnselected: function (event, node) {
-	      $('#selectable-output').prepend('<p>' + node.text + ' was unselected</p>');
-	    }
-	  });
-	};
-	initSelectableTree();
+var sendVarsToBackend = function(vars){
+	// Send calls from menu to backend
+	$.ajax({
+	  type: 'POST',
+	  url: 'postmenu/',
+	  data: {
+	  	//msg: 'Hello my friend!',
+	  	msg: vars,
+	  },
+	  success: function(response){
+	  	$('#selectable-output').html(response);
+	  	console.log('Successful response from backend!');
+	  	console.log(response);
+	  },
+	  error: function(response){
+	  	$('#selectable-output').html('Error (ajax post): Unable to receive data from server.');
+	  }
+	}); // ajax post
+}
 
-})
-.fail(function(){
-	console.log("Fail: Menu json was not used.");
-})
-.always(function(){
-	console.log("Complete: Finished taking menu.");
+var initSelectableTree = function() {
+  return $('#treeview-selectable').treeview({
+  	color: "#428bca",
+	data: menuItems,
+    multiSelect: $('#chk-select-multi').is(':checked'),
+    onNodeSelected: function(event, node) {
+      $('#selectable-output').prepend('<p>' + node.text + ' was selected</p>');
+	  //console.log(node.text);
+	  sendVarsToBackend(node.text);
+    },
+    onNodeUnselected: function (event, node) {
+      $('#selectable-output').prepend('<p>' + node.text + ' was unselected</p>');
+	  //console.log(node.text);
+    }
+  });
+};
+
+var menuItems = {};
+var buildMenu = function(){
+	$.getJSON("/static/esearch/json/test_menu.json", function(menu_data){
+		//console.log("Success: Menu json will be used.");
+	})
+	.done(function(menu_data){
+		menuItems = JSON.stringify(menu_data);
+		//console.log(menuItems);
+		initSelectableTree()
+	})
+	.fail(function(){
+		console.log("Fail: Menu json was not used.");
+	})
+	.always(function(){
+		//console.log("Complete: Finished taking menu.");
+	});
+};
+buildMenu();
+
+
+var $selectableTree = initSelectableTree();
+
+// Form functionality
+var findSelectableNodes = function() {
+  return $selectableTree.treeview('search', [ $('#input-select-node').val(), { ignoreCase: false, exactMatch: false } ]);
+};
+var selectableNodes = findSelectableNodes();
+
+$('#chk-select-multi:checkbox').on('change', function () {
+  console.log('multi-select change');
+  $selectableTree = initSelectableTree();
+  selectableNodes = findSelectableNodes();          
 });
 
+// Select/unselect/toggle nodes
+$('#input-select-node').on('keyup', function (e) {
+  selectableNodes = findSelectableNodes();
+  $('.select-node').prop('disabled', !(selectableNodes.length >= 1));
+});
 
-//var $selectableTree = initSelectableTree();
+$('#btn-select-node.select-node').on('click', function (e) {
+  $selectableTree.treeview('selectNode', [ selectableNodes, { silent: $('#chk-select-silent').is(':checked') }]);
+});
 
-//// Form functionality
-//var findSelectableNodes = function() {
-//  return $selectableTree.treeview('search', [ $('#input-select-node').val(), { ignoreCase: false, exactMatch: false } ]);
-//};
-//var selectableNodes = findSelectableNodes();
-//
-//$('#chk-select-multi:checkbox').on('change', function () {
-//  console.log('multi-select change');
-//  $selectableTree = initSelectableTree();
-//  selectableNodes = findSelectableNodes();          
-//});
+$('#btn-unselect-node.select-node').on('click', function (e) {
+  $selectableTree.treeview('unselectNode', [ selectableNodes, { silent: $('#chk-select-silent').is(':checked') }]);
+});
 
-//// Select/unselect/toggle nodes
-//$('#input-select-node').on('keyup', function (e) {
-//  selectableNodes = findSelectableNodes();
-//  $('.select-node').prop('disabled', !(selectableNodes.length >= 1));
-//});
-//
-//$('#btn-select-node.select-node').on('click', function (e) {
-//  $selectableTree.treeview('selectNode', [ selectableNodes, { silent: $('#chk-select-silent').is(':checked') }]);
-//});
-//
-//$('#btn-unselect-node.select-node').on('click', function (e) {
-//  $selectableTree.treeview('unselectNode', [ selectableNodes, { silent: $('#chk-select-silent').is(':checked') }]);
-//});
-//
-//$('#btn-toggle-selected.select-node').on('click', function (e) {
-//  $selectableTree.treeview('toggleNodeSelected', [ selectableNodes, { silent: $('#chk-select-silent').is(':checked') }]);
-//});
+$('#btn-toggle-selected.select-node').on('click', function (e) {
+  $selectableTree.treeview('toggleNodeSelected', [ selectableNodes, { silent: $('#chk-select-silent').is(':checked') }]);
+});
 
 });
